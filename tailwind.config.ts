bc8075df0b5db16906978ec7e7c9ff8ca4cceb1b
Manon Kim <kim.manon2@gmail.com>
2024-12-16 17:00:23 +0900

    [chore] tailwind setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 
+const colors = require('tailwindcss/colors')
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +11,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#020617',
+        secondary: '#d7ff00',
+        fontPrimary: '#333333',
+        fontSecondary: '#b6b3b3',
+        bgPrimary: '#f4f6f9',
+        white: colors.white,
+        black: colors.black,
+      },      
+      spacing: {
+        xs: '4px',
+        sm: '8px',
+        md: '12px',
+        lg: '16px',
+        xl: '24px',
+        xxl: '32px',
+      },
+      borderRadius: {
+        box: '32px',
+        btn: '36px',
       },
     },
   },
