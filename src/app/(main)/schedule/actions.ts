98a5d7e51b869d5b006049592d309cefb4f35d5f
Manon Kim <kim.manon2@gmail.com>
2025-01-24 16:25:55 +0900

    [refactor] useActionState (form action)

--- src/app/(main)/schedule/actions.ts
+++ src/app/(main)/schedule/actions.ts
@@ -3,7 +3,16 @@
 import db from '@/utils/db'
 import { getUserInfo } from '@/utils/supabase/actions'
 
-export const createTask = async (content: string) => {
+export const createTask = async (
+  prev: ITask[] | undefined,
+  formData: FormData,
+) => {
+  const content = formData.get('content') as string
+
+  if (!content || content.trim() === '') {
+    return
+  }
+
   try {
     const user = await getUserInfo()
 
@@ -22,6 +31,8 @@ export const createTask = async (content: string) => {
         id: true,
       },
     })
+
+    return await getTask()
   } catch (error) {
     console.error('Error creating task:', error)
     throw new Error('Task creation failed.')
@@ -52,3 +63,19 @@ export const getTask = async (): Promise<ITask[]> => {
 
   return tasks
 }
+
+// export const updateTask = async (prev: void | null, formData: FormData) => {
+//   const id = Number(formData.get('id'))
+//   const completed = formData.get('completed')
+//   const content = formData.get('content')
+
+//   try {
+//     await db.task.update({
+//       where: { id },
+//       data: { completed, content },
+//     })
+//   } catch (error) {
+//     console.error('Error updating task:', error)
+//     throw new Error('Task update failed.')
+//   }
+// }
--- src/app/(main)/schedule/components/TaskList.tsx
+++ src/app/(main)/schedule/components/TaskList.tsx
@@ -1,56 +1,35 @@
 'use client'
 
-import React, { useState } from 'react'
-import { createTask, getTask, ITask } from '../actions'
+import React, { useActionState } from 'react'
+
+import { createTask, ITask } from '../actions'
 import { PlusIcon } from '@heroicons/react/24/outline'
 import Checkbox from '@/components/Checkbox'
 
 const TaskInput = ({ tasks }: { tasks: ITask[] }) => {
-  const [inputTask, setInputTask] = useState('')
-  const [taskList, setTaskList] = useState<ITask[]>(tasks)
-
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputTask(event.target.value)
-  }
-
-  const handleOnSubmit = async () => {
-    if (!inputTask.trim()) return
-
-    await createTask(inputTask)
-
-    const newTask = await getTask()
-    setTaskList(newTask)
-    setInputTask('')
-  }
-
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      handleOnSubmit()
-    }
-  }
+  const [state, formAction, isPending] = useActionState(createTask, tasks)
 
   return (
     <div className="flex flex-col justify-between">
       <div className="overflow-y-scroll h-[300px]">
-        {taskList?.map((task) => (
-          <Checkbox key={task.id} text={task.content} />
-        ))}
+        {state?.map((task) => <Checkbox key={task.id} text={task.content} />)}
       </div>
-
-      <label className="input flex items-center gap-2">
-        <input
-          onChange={handleOnChange}
-          onKeyDown={handleKeyPress}
-          name="content"
-          value={inputTask}
-          type="text"
-          placeholder="Add your task"
-          className="input-ghost input-sm w-full max-w-xs"
-        />
-        <button onClick={handleOnSubmit} aria-label="Add Task">
-          <PlusIcon className="w-4 cursor-pointer" />
-        </button>
-      </label>
+      <form action={formAction}>
+        <label className="input flex items-center gap-2">
+          <input
+            // onChange={handleOnChange}
+            // onKeyDown={handleKeyPress}
+            name="content"
+            // value={inputTask}
+            type="text"
+            placeholder="Add your task"
+            className="input-ghost input-sm w-full max-w-xs"
+          />
+          <button aria-label="Add Task" type="submit" disabled={isPending}>
+            <PlusIcon className="w-4 cursor-pointer" />
+          </button>
+        </label>
+      </form>
     </div>
   )
 }
