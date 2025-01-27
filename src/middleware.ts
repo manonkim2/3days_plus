8e1f46dfba6eab358e52396209ad120875075b03
Manon Kim <kim.manon2@gmail.com>
2025-01-27 18:36:44 +0900

    [fix] middleware error - randomly logged out

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { serverCreateClient } from './utils/supabase/server'
+import { createServerClient } from '@supabase/ssr'
 
 interface Routes {
   [key: string]: boolean
@@ -16,8 +16,38 @@ const publicOnlyUrl: Routes = {
 }
 
 export const middleware = async (request: NextRequest) => {
+  let supabaseResponse = NextResponse.next({
+    request,
+  })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value),
+          )
+          supabaseResponse = NextResponse.next({
+            request,
+          })
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options),
+          )
+        },
+      },
+    },
+  )
+
+  // IMPORTANT: Avoid writing any logic between createServerClient and
+  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
+  // issues with users being randomly logged out.
+
   try {
-    const supabase = await serverCreateClient()
     const {
       data: { user },
     } = await supabase.auth.getUser()
@@ -31,6 +61,8 @@ export const middleware = async (request: NextRequest) => {
     console.error('Middleware Error:', error)
     return NextResponse.redirect(new URL('/error', request.url))
   }
+
+  return supabaseResponse
 }
 
 export const config = {
