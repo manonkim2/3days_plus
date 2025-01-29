310388539cfa6c566b62884933f4dd1c6ec697ab
Manon Kim <kim.manon2@gmail.com>
2025-01-29 22:47:01 +0900

    [feat] shadcn/ui setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,61 @@
-import type { Config } from "tailwindcss";
-import daisyui from "daisyui";
+import type { Config } from 'tailwindcss'
+import daisyui from 'daisyui'
+import tailwindcssAnimate from 'tailwindcss-animate'
 
 export default {
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#020617',
-        secondary: '#C6FB50',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
         fontPrimary: '#333333',
         fontSecondary: '#b6b3b3',
         bgPrimary: '#f5f5f5',
         white: '#ffffff',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
       },
       spacing: {
         xs: '4px',
@@ -28,15 +68,19 @@ export default {
       fontSize: {
         '3xl': ['32px', '52px'],
         '2xl': ['24px', '36px'],
-        'xl': ['18px', '24px'],
-        'base': ['14px', '20px'],
-        'sm': ['10px', '16px']
-      }
+        xl: ['18px', '24px'],
+        base: ['14px', '20px'],
+        sm: ['10px', '16px'],
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
     },
   },
-  plugins: [daisyui]
-  ,
+  plugins: [daisyui, tailwindcssAnimate],
   daisyui: {
-    themes: ['emrald']
+    themes: ['emrald'],
   },
-} satisfies Config;
+} satisfies Config
