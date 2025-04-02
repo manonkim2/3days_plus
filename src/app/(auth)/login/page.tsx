0f8287cbc165183443fc4f93799549aeb438996a
Manon Kim <kim.manon2@gmail.com>
2025-04-02 12:58:43 +0900

    [fix] oauth login error

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,29 @@
 'use client'
 
-import Link from 'next/link'
 import GoogleIcon from '@/components/icons/GoogleIcon'
 import KakaoIcon from '@/components/icons/KakaoIcon'
 import { Input } from '@/components/ui'
 import { KeyRound, Mail } from 'lucide-react'
+import { supabase } from '@/utils/supabase/client'
 
 const Login = () => {
+  const handleOAuthLogin = async (provider: 'google' | 'kakao') => {
+    const redirectTo =
+      process.env.NODE_ENV === 'development'
+        ? 'https://localhost:3000/oauth/complete'
+        : 'https://3daysplus.vercel.app/oauth/complete'
+
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: { redirectTo },
+    })
+
+    if (error) {
+      alert('로그인에 실패했습니다. 다시 시도해 주세요.')
+      console.error('[OAuth Error]:', error.message)
+    }
+  }
+
   return (
     <div className="w-[400px] h-[500px]">
       <span className="text-xl pt-sm">Login</span>
@@ -25,12 +42,12 @@ const Login = () => {
       </div>
 
       <div className="flex gap-sm justify-center items-center py-md">
-        <Link href="/oauth/google">
+        <button onClick={() => handleOAuthLogin('google')}>
           <GoogleIcon />
-        </Link>
-        <Link href="/oauth/kakao">
+        </button>
+        <button onClick={() => handleOAuthLogin('kakao')}>
           <KakaoIcon />
-        </Link>
+        </button>
       </div>
 
       {/* <div className="flex justify-center mt-xl">
