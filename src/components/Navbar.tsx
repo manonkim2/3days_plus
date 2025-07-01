bbf3199868737352614edf7e102143801a9da086
Manon Kim <kim.manon2@gmail.com>
2025-07-01 23:58:27 +0900

    [fix] fix build error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,19 @@
-export const dynamic = 'force-dynamic'
-
 import Link from 'next/link'
 import Image from 'next/image'
-import { CircleUser } from 'lucide-react'
+
 import { cn } from '@/utils/cn'
 import NavButtons from './NavButtons'
-import { getUserInfo } from '@/lib/supabase/actions'
+import UserImage from './UserImage'
 
 export interface UserInfo {
   id: string
-  name: string
   email: string
+  name: string
   image_url: string | null
   social: string
 }
 
 const Navbar = async () => {
-  const user = await getUserInfo()
-
   return (
     <nav
       className={cn(
@@ -41,22 +37,7 @@ const Navbar = async () => {
 
       <NavButtons />
 
-      <Link className="rounded-full overflow-hidden" href="/profile">
-        {user?.image_url ? (
-          <Image
-            src={user.image_url || ''}
-            alt="user_image"
-            width={36}
-            height={36}
-          />
-        ) : (
-          <CircleUser
-            className="h-8 w-8 opacity-70"
-            strokeWidth={1.3}
-            aria-label="user_icon"
-          />
-        )}
-      </Link>
+      <UserImage />
     </nav>
   )
 }
